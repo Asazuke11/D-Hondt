dc58497536b400819044de1887a4fca17a92e75b
あさづけ <quusan3@gmail.com>
2019-07-19 17:36:15 +0900

    修正19/07/19

--- d_Hondt.js
+++ d_Hondt.js
@@ -1,18 +1,35 @@
 'use strict';
 
-/*********************************
-*@param {number} seat:比例区改選数
-*********************************/
-
-const seats = 48;
-
 /*************************************************
+*@param {number} seat:比例区改選数
 *@param {Array} party_name: 政党と得票数を入れた配列
 *@param {string} name:政党名
 *@param {number} counter:議席数カウント用
 *@param {number} vetes:得票数
 *@param {string} official:正式名
-*************************************************/
+*@param {Map} D_HondtMap:ドント式の計算結果を入れる連想配列
+*@param {string} key:政党名 + division　(同じ党での上書きを避けるためdivsion文字列追加してます)
+*@param {number} division:ドント式で使う除法の係数
+*@param {number} value:ドント式計算結果数
+*{{"自民1" => Object}
+*key: "自民1"
+*value: {D_Hond_result: 20114788.264, official: "自由民主党"}}
+
+*@param {array} rankingArray:
+*D_HondtMapを配列に変換、獲得票上位順にソートした物が入る配列
+*実行後の配列
+*[["自民1", 20114788.264],["民進1", 11751015.174],...]
+
+*各政党の獲得議席をカウント
+*innerHTMLに送る変数の値を追加していくfor文
+*@param {string} voteList:innerHTMLに送る変数。
+
+*@param {string} seatList:innerHTMLで画面に表示される文字列
+*@param {Array} ranking_party_name:counter数で上位順ソート
+**************************************************************/
+
+
+const seats = 48;
 const party_name = [
   { name: '自民', counter: 0, votes: 20_114_788.264, official: "自由民主党" },
   { name: '民進', counter: 0, votes: 11_751_015.174, official: "民進党" },
@@ -28,15 +45,6 @@ const party_name = [
   { name: '支持', counter: 0, votes: 647_071.670, official: "支持政党なし" }
 ];
 
-/************************************************************
-*@param {Map} D_HondtMap:ドント式の計算結果を入れる連想配列
-*{{"自民1" => Object}
-*key: "自民1"
-*value: {D_Hond_result: 20114788.264, official: "自由民主党"}}
-*@param {string} key:政党名 + division　(同じ党での上書きを避けるためdivsion文字列追加してます)
-*@param {number} division:ドント式で使う除法の係数
-*@param {number} value:ドント式計算結果数
-************************************************************/
 let D_HondtMap = new Map();
 for (let i = 0; i < party_name.length; i++) {
   for (let division = 1; division <= seats; division++) {
@@ -49,24 +57,14 @@ for (let i = 0; i < party_name.length; i++) {
   };
 };
 
-/********************************************************
-*@param {array} rankingArray:
-*D_HondtMapを配列に変換、獲得票上位順にソートした物が入る配列
-*↓実行後の配列
-*[["自民1", 20114788.264],["民進1", 11751015.174],...]
-********************************************************/
+
 const rankingArray = Array.from(D_HondtMap)
   .sort((pair1, pair2) => {
     return pair2[1].D_Hond_result - pair1[1].D_Hond_result;
   });
 
-/**********************************************
-*各政党の獲得議席をカウント
-*innerHTMLに送る変数の値を追加していくfor文
-*@param {string} voteList:innerHTMLに送る変数。
-***********************************************/
-let voteList = ("<p><b>ドント計算内訳</b></p>");
 
+let voteList = ("<p><b>ドント計算内訳</b></p>");
 for (let i = 0; i < seats; i++) {
   if (rankingArray[i][0].slice(0, 2) === '自民') {
     party_name[0].counter++;
@@ -107,10 +105,6 @@ for (let i = 0; i < seats; i++) {
   voteList += ("<p>" + (i + 1) + "." + rankingArray[i][1].official + "：" + rankingArray[i][1].D_Hond_result + "</p>");
 }
 
-/*****************************************************************
-*@param {string} seatList:innerHTMLで画面に表示される文字列
-*@param {Array} ranking_party_name:counter数で上位順ソート
-******************************************************************/
 let seatList = ("<p><b>各政党獲得議席</b></p>");
 
 const ranking_party_name = Array.from(party_name)
@@ -123,7 +117,7 @@ ranking_party_name.forEach((value, index) => {
 });
 
 let list_plain = ("<p><b>開票結果</b></p>");
-party_name.forEach((value, index) => {
+party_name.forEach((value) => {
   list_plain += ("<p>" + value.official + `<br>` + value.votes + "</p><hr>");
 });
 
@@ -131,3 +125,38 @@ document.getElementById("seat").innerHTML = `<p><b>第24回参議院議員通常
 document.getElementById("list-plain").innerHTML = list_plain;
 document.getElementById("list").innerHTML = voteList;
 document.getElementById("result-seat").innerHTML = seatList;
+
+
+//総票数確認テスト//
+//　HPで公表されている総票数=56_007_352.842
+
+let total = 0;
+party_name.forEach((value) => {
+  total += value.votes;
+});
+
+if((Math.round(total * 1000) /1000) === 56_007_352.842){
+// 総務省データが小数点３位で四捨五入してるため↑↑//
+// Math.round *1000) / 1000 使用してます    　//
+  console.log('総票数確認テスト:true');
+}else{
+  console.log('総票数確認テスト:false');
+}
+
+//改選数とカウント総数の確認テスト
+//改選議席４８
+if(48 === seats){
+  console.log('改選議席数の確認:true');
+}else{
+  console.log('改選議席数の確認:false');
+}
+
+let total_seat = 0;
+party_name.forEach((value) => {
+  total_seat += value.counter;
+});
+if(total_seat === seats){
+  console.log('改選議席数合計の確認:true');
+}else{
+  console.log('改選議席数合計の確認:false');
+}
